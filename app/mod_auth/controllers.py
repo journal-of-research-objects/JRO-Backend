941bdcaebad5266d5604159eac1cf9d9e6495069
Brayan Mauricio Rodriguez Rivera <bryan95112@gmail.com>
2019-07-05 16:55:06 -0500

    signin method POST requesting to ORCiD the user data. Tested

--- app/mod_auth/controllers.py
+++ app/mod_auth/controllers.py
@@ -19,20 +19,20 @@ CORS(mod_auth)
 @mod_auth.route('/login/', methods=['GET', 'OPTIONS'])
 def signin():
     auth_code = request.args.get('orcid_auth_code')
-    # params = {'client_id': conf.ORCID_CLIENT_ID,
-    #           'client_secret': conf.ORCID_SECRET,
-    #           'grant_type': 'authorization_code',
-    #           'code': auth_code,
-    #           'redirect_uri': conf.ORCID_REDIRECT_URL
-    #           }
-    # data = urllib.urlencode(params)
-    # req = urllib2.Request(conf.ORCID_API_URL, data)
-    # response = urllib2.urlopen(req)
-    # user_data = json.loads(response.read())
-    # if not user_exists(user_data['orcid']):
-        # create_user(user_data['orcid'], user_data['name'], None, user_data['access_token'])
-    # return jsonify(user_data)
-    return auth_code
+    params = {'client_id': conf.ORCID_CLIENT_ID,
+              'client_secret': conf.ORCID_SECRET,
+              'grant_type': 'authorization_code',
+              'code': auth_code,
+              'redirect_uri': conf.ORCID_REDIRECT_URL
+              }
+    data = urllib.parse.urlencode(params)
+    data = data.encode('ascii') # data should be bytes
+    req = urllib.request.Request(conf.ORCID_API_URL+"token", data)
+    with urllib.request.urlopen(req) as response:
+        user_data = json.loads(response.read())
+        if not user_exists(user_data['orcid']):
+            create_user(user_data['orcid'], user_data['name'], None, user_data['access_token'])
+        return jsonify(user_data)
 
 
 def user_exists(orcid):
