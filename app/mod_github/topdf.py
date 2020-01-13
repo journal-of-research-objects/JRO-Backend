ce499b760c61c3e9de425b895557ded6bd6be49a
Brayan <brayanrodbajo@gmail.com>
2020-01-13 14:36:49 -0500

    adjusting to the changes in API of the PDF generation

--- app/mod_github/topdf.py
+++ app/mod_github/topdf.py
@@ -2,10 +2,11 @@ import os
 import random
 import requests
 import time
+import json
 from docs import conf
 
 MD_PATH = "paper.md"
-REF_PATH = "references.bib"
+REF_PATH = "paper.bib"
 PDF_PATH = "paper.pdf"
 
 #path with slash at the end
@@ -13,24 +14,32 @@ def verify_files_pdf(path):
     return os.path.exists(path+MD_PATH) and os.path.exists(path+REF_PATH)
 
 def create_pdf_file(path, repo_url):
-    ran = random.randrange(10**80)
-    myhex = "%064x" % ran
-    hash = myhex[:24]
     url = conf.WHEDON_URL
     payload = {'repository': repo_url,
+    'branch': 'master',
     'journal': 'biohackrxiv',
     'commit': 'Compile paper',
-    'sha': hash
+    'retid': 'true'
     }
-    requests.post(url, data = payload) # compile paper
-
-    params = {'id': hash}
+    hdr= {
+        'Content-Type':'application/json'
+    }
+    resp_id = requests.post(url, params = payload, headers=hdr) # compile paper
+    print(resp_id.text)
+    job_id = json.loads(resp_id.text)['job_id']
+    
+    time.sleep(6) 
+    params = {'id': job_id}
     resp = requests.get(url, params=params, stream=True)
     content_type = resp.headers.get('content-type')
+    counter = 1
     while not 'application/pdf' in content_type: # the paper is not compiled instantly
         time.sleep(3) 
         resp = requests.get(url, params=params, stream=True)
         content_type = resp.headers.get('content-type')
+        if counter == 18:
+            raise Exception ("error generating pdf")
+        counter+=1
 
     with open(path+PDF_PATH, 'wb') as f:
         f.write(resp.raw.read())
